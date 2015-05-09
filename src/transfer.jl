cc1f0b1bd9878730ffcb91949d3f33ca12207759
Benjamin Piwowarski <benjamin@bpiwowar.net>
2015-05-09 13:46:35 +0200

    Removed unneeded type parameters

--- src/transfer.jl
+++ src/transfer.jl
@@ -1,14 +1,14 @@
 @doc doc"Hyperbolic tangent transfer layer" ->
-type TanH{D<:Device, F<:Float}
+type TanH
     output::RealArray
     grad_input::RealArray
 end
 
-function forward!{D<:Device, F<:Float}(m::TanH{D,F}, input::RealMatrix)
+function forward!(m::TanH, input::RealMatrix)
     self.output = tanh(input)
 end
 
-function compute_inputgradient!{D<:Device, F<:Float}(m::TanH{D,F}, grad_output::RealMatrix)
+function compute_inputgradient!(m::TanH, grad_output::RealMatrix)
     @simd for i = eachindex(grad_output.values)
         self.grad_input[i] = (one(F) - m.output[i] * m.output[i]) * grad_output[i]
     end
