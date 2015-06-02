57840c012a2a2c5700934a345e312f53d1e836e5
Benjamin Piwowarski <benjamin@bpiwowar.net>
2015-06-03 00:10:07 +0200

    Cleaned up documentation

--- src/optimization.jl
+++ src/optimization.jl
@@ -17,26 +17,8 @@ function optimize!(s::Optimizer, m::Layer)
   end
 end
 
-# -# Stochastic Gradient
-#= A plain implementation of SGD
-ARGS:
-- `opfunc` : a function that takes a single input (X), the point
-             of a evaluation, and returns f(X) and df/dX
-- `x`      : the initial point
-- `config` : a table with configuration parameters for the optimizer
-- `config.learningRate`      : learning rate
-- `config.learningRateDecay` : learning rate decay
-- `config.weightDecay`       : weight decay
-- `config.weightDecays`      : vector of individual weight decays
-- `config.momentum`          : momentum
-- `config.dampening`         : dampening for momentum
-- `config.nesterov`          : enables Nesterov momentum
-- `state`  : a table describing the state of the optimizer; after each
-             call the state is modified
-- `state.learningRates`      : vector of individual learning rates
 
-=#
-
+@doc doc"Stochatic gradient" ->
 type StochasticGradient <: Optimizer
     learningRate::Float64
     # learningRateDecay::Float64
